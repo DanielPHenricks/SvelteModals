e48097ca456c7fd7b042a1001dba52978c4787b3
daniel <daniel.p.henricks@vanderbilt.edu>
2023-02-08 17:37:14 -0600

    Adding initial files and default modal

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Modal from "./lib/Modal.svelte";
+
+    const exampleContent = {
+        modalTitle: "Info about Vandy Sports.",
+        modalText: "These are the future sports games at Vandy: ",
+    };
+</script>
+
+<!-- An example of a default modal. -->
+<Modal />
+
+<!-- An example of a modal with custom content. -->
+<Modal {...exampleContent} />
